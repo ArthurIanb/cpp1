f4795c278f0242d28c91fb70f27492b98c3304d6
ArthurIanb <97800390+ArthurIanb@users.noreply.github.com>
2022-05-11 19:43:31 +0500

    Add files via upload
    eng
    p 123
    ex 5
    dicata

--- Source.cpp
+++ Source.cpp
@@ -1,123 +1,75 @@
-
 #include <iostream>
-#include <vector>
-
+#include <map>
+#include <ctime>
+#include <string>
 using namespace std;
 
-class coord
+
+map<string, string> phrases = 
 {
-private:
-	float xPos;
-	float yPos;
-
-public:
-	coord()
-	{
-		xPos = 0;
-		yPos = 0;
-	}
-	coord operator*(coord s)
-	{
-		xPos *= s.xPos;
-		yPos *= s.yPos;
-		return *this;
-	}
-	coord(float x, float y) :xPos(x), yPos(y) {}
-	void SetCoords()
-	{
-		
-		cout << "X:";
-		cin >> xPos;
-		cout << "Y:";
-		cin >> yPos;
-	}
-	coord operator- (coord s)
-	{
-		coord out;
-		out.xPos = this->xPos - s.xPos;
-		out.yPos = this->yPos - s.yPos;
-		return out;
-	}
-	void showCoords()
-	{
-		cout << "X:" << xPos << endl << "Y:" << yPos;
-	}
-	bool operator<(coord x)
-	{
-		return this->xPos - x.xPos < x.xPos - this->xPos;
-	}
-
-	bool operator>(coord x)
-	{
-		return this->xPos - x.xPos > x.xPos - this->xPos;
-	}
-	coord operator+(coord s)
-	{
-		coord out;
-		out.xPos = xPos + s.xPos;
-		out.yPos = yPos + s.yPos;
-		return out;
-	}
-
-	void operator=(coord x)
-	{
-		this->xPos = x.xPos;
-		this->yPos = x.yPos;
-	}
-
-	float sum()
-	{
-		return xPos + yPos;
-	}
+	{"listening to cds", "слушать диски"},
+	{"going to art galleries", "ходить в галереи"},
+	{"painting pictures", "рисовать картины"},
+	{"reading books/magazines", "читать книги / журналы"},
+	{"going to the cinema", "ходить в кино"},
+	{"eating out", "обедать"},
+	{"designing your own clothes", "дизайнить собственную одежду"},
+	{"writing novels", "писать романы"},
+	{"watching dvds", "смотреть двд"},
+	{"going to fashion shows", "ходить на показы моды"},
+	{"watching a game", "смотреть игру"},
+	{"playing computer games", "играть в комп игры"},
+	{"working out at a gym", "тренироваться в зале"},
+	{"playing a musical instrument", "играть на музыкальном инструменте"}
+	
 
 };
 
-coord findClose(coord myCoord, coord* money, int size)
+map<string, string>::iterator getPhraseNumber(int k)
 {
-	coord closest = money[0];
-	
-	for (int i = 1; i < size; ++i)
-	{
-		if (closest < coord(0, 0))
-			closest = closest * coord(-1, -1);
-		if (myCoord - closest > myCoord - money[i])
-			closest = money[i];
-
-	}
-	return closest;
+	setlocale(0, "ru");
+	map<string, string>::iterator iter = phrases.begin();
+	for (int i = 0; i < k-1; i++)
+		iter++;
+	return iter;
 }
 
-coord* setMoney(int size)
+int getRandTo14()
 {
-
-	coord* money = new coord[size];
-	for (int i = 0; i < size; i++)
-		money[i].SetCoords();
-	
-	return money;
+	return rand() % 14+1;
 }
 
 void Work()
 {
-	cout << "Set you coordinates\n";
-	coord myCoord;
-	myCoord.SetCoords();
-	int size;
-	cout << "Enter size: ";
-	cin >> size;
-	coord* money = setMoney(size);
-	coord closest = findClose(myCoord, money, size);
-	cout << "Closest coords is\n";
-	closest.showCoords();
+	char st[200];
+	setlocale(0, "ru");
+	while (true) {
+		map<string, string>::iterator randPhrases = getPhraseNumber(getRandTo14());
+		cout << "Phrase on russia: " << randPhrases->second;
+		cout << "\nAns: ";
+		
+		cin.getline(st, 200, '\n');
+		if (strcmp(st, "s")==0)
+			continue;
+		if(strcmp(st, "e") == 0)
+			break;
+		if (st == randPhrases->first)
+			cout << "Correct\n";
+		else
+		{
+			cout << "The correct: " << randPhrases->first << '\n';
+		}
+	}
 }
 
 
 
 int main()
 {
+	srand(time(NULL));
+	
 	Work();
+	
 	return 0;
 }
 
-//  5 5 2 4 4 1 1
-//	3 3 2 4 4 1 1
